e8890f1b2e65127f86bae0f6f8e51dc56a7c000b
wurrm <wurrm@pm.me>
2020-01-02 17:33:36 +0000

    Add some function explanations and do simple deobfuscations

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+// Observations:
+//  a 'flagman' is at most 4 characters high (N = 4), and 8 wide (Y = 8).
+//  all sequences (terminated with newline or space in input) end with the same flag position
+
 char *T="IeJKLMaYQCE]jbZRskc[SldU^V\\X\\|/_<[<:90!\"$434-./2>]s";
 char K[3][1000];
 char *F;
@@ -19,7 +23,7 @@ char D;
 
 X()
 {
-    r[0] = W;
+    r[0] = ' ';
     r[1] = x + 1 + Y;
     r[2] = *Q + 2;
     r[3] = M[1-(x&1)][1];
@@ -32,52 +36,103 @@ X()
     }
 }
 
-E()
+E() // Calls X() while A ^= 1
 {
-    A||X(x=0,g=J),x=7&(*T>>A*3),J[(x[F]-W-x)^A*7]=Q[x&3]^A*(*M)[2+(x&1)],g=J+((x[k]-W)^A*7)-A,g[1]=(*M)[*g=M[T+=A,1][x&1],x&1],(A^=1)&&(E(),J+=W);
+    A||X(x=0,g=J),x=7&(*T>>A*3),J[(x[F]-W-x)^A*7]=Q[x&3]^A*(*M)[2+(x&1)],g=J+((x[k]-W)^A*7)-A,g[1]=(*M)[*g=M[T+=A,1][x&1],x&1]
+
+    if (A^=1)
+    {
+	E();
+	J += W;
+    }
 }
 
-l()
+l() // Calls E() while q.
 {
-    E(--q && l());
+    --q;
+    E();
+
+    if (q)
+    {
+	l();
+    }
 }
 
-B()
+B() // Likely sets up each flagman
 {
     *J&&B((D=*J,Q[2]<D&&D<k[1]&&(*g++=1),!(D-W&&D-9&&D-10&&D-13)&&(!*r&&(*g++=0),*r=1)||64<D&&D<91&&(*r=0,*g++=D-63)||D>=97&&D<123&&(*r=0,*g++=D-95)||!(D-k[3])&&(*r=0,*g++=12)||D>k[3]&&D<=k[1]-1&&(*r=0,*g++=D-47),J++));
 }
 
-j()
+j() // Writes a single character.
 {
     putchar(A);
 }
 
-b()
+b() // Writes each character for a given flagman.
 {
-    (j(A=(*K)[D*W+r[2]*Y+x]),++x<Y)&&b();
+    A = K[0][D*W+r[2]*Y+x];
+    j();
+
+    if (++x < Y)
+    {
+	b();
+    }
 }
 
-t()
+t() // Prints a row of characters for a flagman.
 {
-    (j((b(D=q[g],x=0),A=W)),++q<(*(r+1)<Y?*(r+1):Y))&&t();
+    D = q[g];
+    x = 0;
+    b();
+
+    A = ' ';
+    j();
+
+    if (++q < (r[1] < Y ? r[1] : Y))
+    {
+	t();
+    }
 }
 
-R()
+R() // Writes a row of characters.
 {
-    (A=(t(q=0),'\n'),j(),++r[2]<N)&&R();
+    q = 0;
+    t();
+
+    A = '\n';
+    j();
+
+    if (++r[2] < N)
+    {
+	R();
+    }
 }
 
-O()
+O() // Writes characters to screen for a given user input.
 {
-    (j((r[2]=0,R())),r[1]-=q)&&O(g-=-q);
+    r[2] = 0;
+    R();
+    j();
+
+    r[1] -= q;
+    g += q;
+    O();
 }
 
-C()
+C() // Gets user input and sets up input representation, then writes to screen.
 {
-    (J=gets(K[1]))&&C((B(g=K[2]),*r=!(!*r&&(*g++=0)),(*r)[r]=g-K[2],g=K[2],r[1]&&O()));
+    if (gets(K[1]))
+    {
+	C((B(g=K[2]),*r=!(!*r&&(*g++=0)),(*r)[r]=g-K[2],g=K[2]));
+
+	if (r[1])
+	{
+	    O();
+	}
+    }
 }
 
-main()
+int main()
 {
     C((l((J=(A=0)[K],A[M]=(F=(k=(M[!A]=(Q=T+(q=(Y=(W=32)-(N=4))))+N)+2)+7)+7)),Y=N<<(*r=!-A)));
 }
